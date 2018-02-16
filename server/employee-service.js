4757b6174b057e5cc2cffc0af4158c94281eb324
Ahmid <ahd.hjz@gmail.com>
2018-02-16 12:45:06 +0200

    Added the employee service (empty functions)

---
+++ server/employee-service.js
@@ -0,0 +1,26 @@
+const Employee = require('./employee-model');
+const ReadPreference = require('mongodb').ReadPreference;
+
+require('./mongo').connect();
+
+function get(req, res) {
+
+}
+
+function create(req, res) {
+
+}
+
+function search(req, res) {
+
+}
+
+function update(req, res) {
+
+}
+
+function destroy(req, res) {
+
+}
+
+module.exports = { get, create, update, destroy, search };
